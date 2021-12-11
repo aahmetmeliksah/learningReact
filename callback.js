b7460d4646fa597e5c0463f8cc1359541e30bc2d
Ahmet Meliksah <aahmetmeliksah@gmail.com>
2021-12-11 11:14:27 +0300

    used async and await
    learnt how to use async and await keywords

--- callback.js
+++ callback.js
@@ -11,6 +11,34 @@
 
 import fetch from "node-fetch";
 
-fetch("https://jsonplaceholder.typicode.com/users").then((data) =>
-  console.log(data)
-);
+// fetch("https://jsonplaceholder.typicode.com/users").then((data) =>
+//   data.json()).then((users) => {
+//     console.log("fetched users", users);
+
+//     fetch("https://jsonplaceholder.typicode.com/posts/1").then((data) => 
+//       data.json())
+//       .then((posts) => console.log("fetched post" ,posts))
+//     })
+
+async function getData() {
+
+  // fetch users
+  const dataUsers = await (
+    await fetch("https://jsonplaceholder.typicode.com/users")
+    ).json();
+    console.log(dataUsers);
+
+    // fetch post 1
+  const post1 = await (
+    await fetch("https://jsonplaceholder.typicode.com/posts/1")
+  ).json();
+  console.log("post1 is uploaded", post1);
+
+  // fetch post 2
+  const post2 = await (
+    await fetch("https://jsonplaceholder.typicode.com/posts/2")
+  ).json();
+  console.log("post2 is uploaded", post2);
+}
+
+getData();
