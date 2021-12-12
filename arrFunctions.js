10f81be30bb7af863eeb4d253a4e059dd0be5c55
Ahmet Meliksah <aahmetmeliksah@gmail.com>
2021-12-12 09:26:36 +0300

    the end

---
+++ arrFunctions.js
@@ -0,0 +1,30 @@
+/*
+const users = [
+    { 
+        name: "Bob",
+        age: 21 
+    },
+    {
+        name: "Billy",
+        age: 40
+    }, 
+    {
+        name: "Sara",
+        age: 25
+    },
+    {
+        name: "Bob",
+        age: 23
+    }
+];
+
+// users.map((user) => console.log(user));
+
+// const findUser = users.find((user) => user.name === "Bob");
+// console.log(findUser);
+
+// const filtered = users.filter(({name, age}) => age < 30 && name === "Bob");
+// console.log(filtered);
+
+*/
+
